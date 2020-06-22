dc81e059f7a35f5625c67cdf21e9314ef768874e
James Bremner <james@ravenspoint.com>
2020-06-22 14:05:22 -0400

    Another loop limit adjustment

--- src/DDP.cpp
+++ src/DDP.cpp
@@ -24,11 +24,11 @@ std::vector<int> DDP(
 
     std::vector<int> P;          // calculated discretization points
 
-    std::vector<int> c( D );
+    std::vector<int> c( D+1 );
 
-    for( int i = 1; i < (int)d.size(); i++ )
+    for( int i = 0; i < (int)d.size(); i++ )
     {
-        for( int j = d[i]; j < D; j++ )
+        for( int j = d[i]; j <= D; j++ )
         {
             if( c[ j ] < c[ j - d[i] ] + d[ i ] )
                 c[ j ] = c[ j - d[i] ] + d[ i ];
@@ -43,22 +43,37 @@ std::vector<int> DDP(
     return P;
 }
 
-int main()
+void text(
+    int D,
+    std::vector<int>& d,
+    std::vector<int>& P )
 {
-    cout << "DDP" << endl;
-
-    int D = 3;
-    std::vector<int> d  = { 1,2 };
-
-    std::vector<int> P = DDP( D, d );
-
     std::cout << "D = " << D << "\ndi = ";
     for( int di : d )
         std::cout << di << " ";
     std::cout << "\nP = ";
     for( int p : P )
         std::cout << p << " ";
-    std::cout << "\n";
+    std::cout << "\n\n";
+}
+
+int main()
+{
+    cout << "DDP" << endl;
+
+    int D = 10;
+    std::vector<int> d  = { 1,2,3,4 };
+
+    std::vector<int> P = DDP( D, d );
+
+    text( D, d, P );
+
+    int D2 = 10;
+    std::vector<int> d2  = { 3,7 };
+
+    P = DDP( D2, d2 );
+
+    text( D2, d2, P );
 
     return 0;
 }
