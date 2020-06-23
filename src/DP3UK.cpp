2d1fb82ac122535c4286198fceade8fc54d1f46e
James Bremner <james@ravenspoint.com>
2020-06-22 20:56:47 -0400

    Implement remainder of DP3UK

--- src/DP3UK.cpp
+++ src/DP3UK.cpp
@@ -24,12 +24,13 @@ void DP3UK (
     std::vector<std::vector<std::vector<int> > >    G(m,G2);
     std::vector<std::vector<std::vector<int> > >    item(m,G2);
     std::vector<std::vector<std::vector<int> > >    guil(m,G2);
+    std::vector<std::vector<std::vector<int> > >    pos(m,G2);
 
     // cut orientation codes
     const int nil = 0;
-    const int V = 1;
-    const int D = 2;
-    const int H = 3;
+    const int Vert = 1;
+    const int Depth = 2;
+    const int Horz = 3;
 
     for( int i = 0; i < m; i++ )
     {
@@ -56,8 +57,88 @@ void DP3UK (
                     }
                 }
                 std::cout << "Most valuable item in ( "
-                    << Phat[i] <<" "<< Qhat[j] <<" "<< Rhat[k] <<" ) has value "<< G[i][j][k] << "\n";
+                          << Phat[i] <<" "<< Qhat[j] <<" "<< Rhat[k] <<" ) has value "<< G[i][j][k] << "\n";
+            }
+        }
+    }
+    for( int i = 0; i < m; i++ )
+    {
+        for( int j = 0; j < s; j++ )
+        {
+            for( int k = 0; k < u; k++ )
+            {
+                // avoid generating symmetric patterns by considering, in each direction,
+                // r-points up to half of the size of the respective bin
+                int nn;
+                for( int d = 0; d <= i; d++ )
+                {
+                    if( Phat[d] <= Phat[i] / 2 )
+                        nn = d;
+                }
+                for( int x = 0; x <= nn; x++ )
+                {
+                    int t = 0;
+                    for( int d = 0; d <= m; d++ )
+                    {
+                        if( Phat[d] <= Phat[i] - Phat[x] )
+                            t = d;
+                    }
+
+                    if( G[i][j][k] < G[x][j][k]+G[t][j][k] )
+                    {
+                        G[i][j][k] = G[x][j][k]+G[t][j][k];
+                        pos[i][j][k] = Phat[x];
+                        guil[i][j][k] = Vert;  // Vertical cut; parallel to yz-plane
+                    }
+                }
+
+
+                for( int d = 0; d <= j; d++ )
+                {
+                    if( Qhat[d] <= Qhat[j] / 2 )
+                        nn = d;
+                }
+                for( int y = 0; y <= nn; y++ )
+                {
+                    int t = 0;
+                    for( int d = 0; d <= s; d++ )
+                    {
+                        if( Qhat[d] <= Qhat[j] - Qhat[y] )
+                            t = d;
+                    }
+
+                    if( G[i][j][k] < G[i][y][k]+G[i][t][k] )
+                    {
+                        G[i][j][k] = G[i][y][k]+G[i][y][k];
+                        pos[i][j][k] = Qhat[y];
+                        guil[i][j][k] = Depth;  // Depth cut ðvertical; parallel to xy plane
+                    }
+                }
+
+                for( int d = 0; d <= k; d++ )
+                {
+                    if( Rhat[d] <= Rhat[k] / 2 )
+                        nn = d;
+                }
+                for( int z = 0; z <= nn; z++ )
+                {
+                    int t = 0;
+                    for( int d = 0; d <= u; d++ )
+                    {
+                        if( Rhat[d] <= Rhat[k] - Rhat[z] )
+                            t = d;
+                    }
+
+                    if( G[i][j][k] < G[i][j][z]+G[i][j][t] )
+                    {
+                        G[i][j][k] = G[i][j][z]+G[i][j][t];
+                        pos[i][j][k] = Rhat[z];
+                        guil[i][j][k] = Horz;  // Horizontal cut; parallel to xy plane
+                    }
+                }
             }
         }
     }
 }
+
+
